1f25dda6ce3def55c8382f4df614d2b6f45e8147
Chan Chee Tuck <cheetuck_chan@merck.com>
2021-10-24 18:42:57 +0800

    Initial commit

---
+++ .dart_tool/flutter_build/53b1bf07cff8aa5f043b9100e275c471/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:food_web/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
